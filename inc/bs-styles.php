8ed2054dbd358625c380b751af2aabf1c35eea4e
serii <seriiburduja@gmail.com>
2020-06-29 11:40:15 +0300

    add to cart js

--- inc/bs-styles.php
+++ inc/bs-styles.php
@@ -15,6 +15,13 @@ function wc_woocommerce_scripts() {
 	wp_enqueue_script( 'wc-woocommerce-responsinveslides-js', get_template_directory_uri() . '/assets/js/responsiveslides.min.js', [ 'jquery' ], null, true );
 	wp_enqueue_script( 'wc-woocommerce-flexisel-js', get_template_directory_uri() . '/assets/js/jquery.flexisel.js', [ 'jquery' ], null, true );
 	wp_enqueue_script( 'wc-woocommerce-main-js', get_template_directory_uri() . '/assets/js/main.js', [ 'jquery' ], null, true );
+	wp_deregister_script( 'wc-add-to-cart' );
+	wp_enqueue_script( 'wc-add-to-cart', get_template_directory_uri() . '/woocommerce/js/add-to-cart.js', array(
+		'jquery',
+		'woocommerce',
+		'wc-country-select',
+		'wc-address-i18n'
+	), null, true );
 	wp_enqueue_script( 'wc-woocommerce-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
